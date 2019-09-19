46a08016e5addc01e544c3d4386f85f417fb35f1
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-19 12:07:45 -0300

    Configuring Reactotron

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignIn from '../pages/Signin';
-import SignUp from '../pages/SignUp';
+import SignUp from '../pages/Signup';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
