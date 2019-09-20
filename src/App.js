1b2035e8a997f77c6f07a297e101942c9bca0169
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-20 14:28:55 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
