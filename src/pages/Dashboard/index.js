d4f1b010bc3acbecc677a48e01237998a2633557
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-21 19:42:29 -0300

    Navigating between dates dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import en from 'date-fns/locale/en-US';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, 'do MMMM yyyy', { locale: en }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1)); // reducing 1 day
+  }
+  function handleNextDay() {
+    setDate(addDays(date, 1)); // reducing 1 day
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>May, 31th</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
