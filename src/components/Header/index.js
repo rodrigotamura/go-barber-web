dd17e8cfae0e5e4fe010acd85c4b2bf48d7e27fc
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-21 18:44:12 -0300

    Updating profile header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +9,7 @@ import logo from '~/assets/logo-header.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -20,13 +22,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Rodrigo Tamura</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png
-"
-              alt="Rodrigo Tamura"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
